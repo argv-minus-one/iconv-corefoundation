8d762cd66aa12828597043a11ae48c06c908eaf8
argv-minus-one <argv-minus-one@users.noreply.github.com>
2019-05-28 18:58:29 -0700

    Remove now-unnecessary inclusion of iccf.hh from string-utils.hh.

--- src/string-utils.hh
+++ src/string-utils.hh
@@ -4,8 +4,6 @@
 #include "CFHandle.hh"
 #include "napi.hh"
 
-struct Iccf;
-
 /**
  * Makes a `CFStringRef` from the characters in the given `Napi::String`, making 1 to 2 copies.
  */
@@ -17,5 +15,3 @@ CFStringHandle NapiStringToCFString(const Napi::String string);
  * The passed-in string is always copied, so it is safe to `CFRelease` it after this function completes. (That will happen automatically if it's wrapped in a CFHandle.)
  */
 Napi::String CFStringToNapiString(CFStringRef string, Napi::Env env);
-
-#include "iccf.hh"
