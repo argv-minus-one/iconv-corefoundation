ea5d8fb2dd9885ff0195ac2e6a594abb590e9259
Mike Maietta <michaelmaietta@upwork.com>
2021-03-05 08:52:40 -0800

    chore: updating CXXFLAGS to build arm64 which results in a single "fat" binary: > Architectures in the fat file: ./lib/native.node are: x86_64 arm64
    Fixes #2

--- native-debug.mk
+++ native-debug.mk
@@ -1,4 +1,4 @@
-CXXFLAGS := -mmacosx-version-min=10.10 -arch x86_64 -Inode_modules/node-addon-api -I/usr/local/include/node -fno-rtti -fvisibility=hidden -Wall -std=c++17 -DBUILDING_NODE_EXTENSION -g $(CXXFLAGS)
+CXXFLAGS := -mmacosx-version-min=10.10 -arch x86_64 -arch arm64 -Inode_modules/node-addon-api -I/usr/local/include/node -fno-rtti -fvisibility=hidden -Wall -std=c++17 -DBUILDING_NODE_EXTENSION -g $(CXXFLAGS)
 LDFLAGS := $(CXXFLAGS) -bundle -undefined dynamic_lookup -framework CoreFoundation $(LDFLAGS)
 
 lib/native.node: build/iccf.o build/string-utils.o build/StringEncoding.o build/transcode.o
--- native.mk
+++ native.mk
@@ -1,4 +1,4 @@
-CXXFLAGS := -mmacosx-version-min=10.10 -arch x86_64 -Inode_modules/node-addon-api -I/usr/local/include/node -flto -fno-rtti -Os -fvisibility=hidden -Wall -std=c++17 -DBUILDING_NODE_EXTENSION -flto $(CXXFLAGS)
+CXXFLAGS := -mmacosx-version-min=10.10 -arch x86_64 -arch arm64 -Inode_modules/node-addon-api -I/usr/local/include/node -flto -fno-rtti -Os -fvisibility=hidden -Wall -std=c++17 -DBUILDING_NODE_EXTENSION -flto $(CXXFLAGS)
 LDFLAGS := $(CXXFLAGS) -bundle -undefined dynamic_lookup -Wl,-x -framework CoreFoundation -Wl,-dead_strip -g0 $(LDFLAGS)
 
 lib/native.node: build/iccf.o build/string-utils.o build/StringEncoding.o build/transcode.o
